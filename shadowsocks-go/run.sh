58660af18ed4c8827da45c08775555d9fc0e2502
Robin Shi <robinshi@outlook.com>
2017-06-30 23:04:58 +0000

    init commit

---
+++ shadowsocks-go/run.sh
@@ -0,0 +1,1 @@
+docker run -d --env-file=env.list  -p=2015:8388 --name ssserver-go shadowsocks-go
---
+++ shadowsocks-go/start.sh
@@ -0,0 +1,10 @@
+
+echo "==========================="
+echo " ShadowSocks Server is up:"
+echo " port    : $SS_SERVER_PORT"
+echo " password: $SS_PASSWORD"
+echo " method  : $SS_METHOD"
+echo " timeout : $SS_TIMEOUT"
+echo "==========================="
+
+shadowsocks-server -p $SS_SERVER_PORT -k $SS_PASSWORD -m $SS_METHOD -t $SS_TIMEOUT
